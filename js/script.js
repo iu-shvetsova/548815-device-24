476108daebf06884104bba168a18c79698514733
iu-shvetsova <iu-shvetsova69@yandex.ru>
2019-03-22 14:25:24 +0300

    Оживлены слайдеры

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ var inputEmail = modalForm.querySelector("[name=email]");
 
 var servicesButton = document.querySelectorAll(".services-nav__button");
 var servicesItem = document.querySelectorAll(".services__item");
-
+var popularButton = document.querySelectorAll(".popular-nav__item button");
+var popularRadio = document.querySelectorAll(".popular-nav__item");
+var popularItem = document.querySelectorAll(".popular-list__item");
 
 document.querySelectorAll('.modal__close').forEach(function(closeBtn) {
 	closeBtn.addEventListener('click', function() {
@@ -56,12 +58,24 @@ inputEmail.addEventListener("blur", function(evt) {
 	}
 });
 
-var i;
-var servicesLength = servicesItem.length;
-
-for (i = 0; i < servicesLength; i++) {
-	servicesButton[i].addEventListener("click", function(evt) {
-		console.log(i);
+servicesButton.forEach(function(button, index) {
+	button.addEventListener("click", function(evt) {
+	for (var i = 0; i < servicesItem.length; i++) {
+		servicesItem[i].classList.remove("services__item--current");
+		servicesButton[i].classList.remove("services-nav__button--current");
+	}
+	servicesItem[index].classList.add("services__item--current");
+	servicesButton[index].classList.add("services-nav__button--current");
 	});
+});
 
-}
+popularButton.forEach(function(button, index) {
+	button.addEventListener("click", function(evt) {
+		for (var i = 0; i < popularItem.length; i++) {
+			popularItem[i].classList.add("popular-list__item--hidden");
+			popularRadio[i].classList.remove("popular-nav__item--current");
+		}
+		popularItem[index].classList.remove("popular-list__item--hidden");
+		popularRadio[index].classList.add("popular-nav__item--current");
+	});
+});
